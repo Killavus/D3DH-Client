4d9cefc1a2def9f1da072031dcb9c4355466eaf6
Igorek <igor.gajowiak@gmail.com>
2018-03-14 00:42:47 +0100

    cmake fixed

---
+++ src/client/timeService.cpp
@@ -0,0 +1,35 @@
+#include <rpc/server.h>
+
+#include "timeService.h"
+
+TimeService::TimeService(int port)
+    : synchronized(false)
+{
+    rpc::server srv(port);
+        
+    srv.bind("synchronize",
+        [this]() 
+        {
+            time_t deliveryTime = time(nullptr);
+            return synchronize(deliveryTime);
+        });
+    
+    srv.bind("syncFinished", 
+        [this]()
+        {
+            synchronized = true;
+        });
+    
+    srv.async_run();
+}
+
+std::pair<time_t, time_t> TimeService::synchronize(time_t deliveryTime)
+{
+    time_t sendingTime = time(nullptr);
+    return std::make_pair(deliveryTime, sendingTime);
+}
+
+bool TimeService::isSynchronized()
+{
+    return synchronized;
+}
